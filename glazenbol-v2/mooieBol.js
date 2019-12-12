1e4b047251cfbcad35ad3286cdf2ef3bd8ac4467
MarijeHammel <MarijeHammel@users.noreply.github.com>
2019-12-12 11:09:55 +0100

    Idk?

---
+++ glazenbol-v2/mooieBol.js
@@ -0,0 +1,11 @@
+import fortunes from fortunes;
+import reactions from reactions;
+
+// random reactie
+
+const randomPositiveReaction = positiveReaction [Math.floor(Math.random() * positiveReaction.length)] // random 'het gaat goed'
+const randomNegativeReaction = negativeReaction [Math.floor(Math.random() * negativeReaction.length)] // random 'het gaat slecht
+const randomNeutralReaction = neutralReaction [Math.floor(Math.random() * neutralReaction.length)] // random 'het gaat whatever'
+
+// random fortune
+
---
+++ glazenbol-v2/reactions.js
@@ -0,0 +1,38 @@
+// dit zijn de arrays voor de reacties
+
+const positiveReaction = [
+    `Wat goed om te horen! `,
+    `Heel fijn! `,
+    `Houd dat vol! `,
+    `Heel prettig! `,
+    `Wat leuk! `,
+    `Nee écht? `,
+    `Nou, wat goed! `,
+    `Geniet van je goede bui! `,
+    `Heerlijk toch! `,
+    `Heel prettig om dat te horen. `
+]
+
+const negativeReaction = [
+    `Ach gossie, nee! `,
+    `Gut, oh gut. `,
+    `Wat vervelend! `,
+    `Ik ben er voor je! `,
+    `Vergeet niet om hulp te vragen! `,
+    `Heb je een knuffel nodig? `,
+    `Na regen komt zonneschijn. `,
+    `Gut, wat vervelend. `,
+    `Hè, nee! `,
+    `Nou, dat is jammer. `,
+    `Och mensje toch, moet ik thee voor je zetten? `
+]
+
+const neutralReaction = [
+    `Nou, oké! `,
+    `Hoe is het weer vandaag? `,
+    `Je zal vast nog ademen. `,
+    `Ik vind helemal mooi. `,
+    `Als jij het zegt. `,
+    `Nou, nou. Vooruit. `
+]
+
